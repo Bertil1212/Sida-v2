190c615afbfc71a25ae8208da3608239fc3fba8f
Andreas <andreasbertilsson1@gmail.com>
2022-04-03 18:01:28 +0200

    1

--- adminbuy.php
+++ adminbuy.php
@@ -10,12 +10,12 @@
     file_put_contents( $filename , implode( "\n", $lines ) );
     
     
-    header("Location: http://www.andreasb.one/HUST/index.html");
+    header("Location: http://www.andreasb.one/HUST");
     exit;
     
     
 } else {
-    header("Location: http://www.andreasb.one/HUST/index.html");
+    header("Location: http://www.andreasb.one/HUST");
     exti;
     
 }

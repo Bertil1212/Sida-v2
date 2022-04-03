e48cfe1a954f8ba22d97c408ddccd3c86ee1ccf5
Andreas <andreasbertilsson1@gmail.com>
2022-04-03 22:44:32 +0200

    1

--- saveinfo.php
+++ saveinfo.php
@@ -8,9 +8,9 @@
     $lines = file( $filename , FILE_IGNORE_NEW_LINES );
     $lines[$line_i_am_looking_for] = '0';
     file_put_contents( $filename , implode( "\n", $lines ) );
+    echo "Saved";
     
-    
-    header("Location: https://forms.office.com/Pages/ResponsePage.aspx?id=CMLpDBt9rU-Cyxpwj4U9ESxi1HBvv6JOqLt6Ggc3H69UMVJBVk0yMjhONlExT0NRNDZCQko2SjI5Ti4u");
+    //header("Location: https://forms.office.com/Pages/ResponsePage.aspx?id=CMLpDBt9rU-Cyxpwj4U9ESxi1HBvv6JOqLt6Ggc3H69UMVJBVk0yMjhONlExT0NRNDZCQko2SjI5Ti4u");
     exit;
     
     

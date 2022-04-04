d06c4e8feaae5e0dd520e8c8967621cbe073bf45
Andreas <andreasbertilsson1@gmail.com>
2022-04-04 07:36:54 +0200

    Admin panle and accept working

--- adminsell.php
+++ adminsell.php
@@ -10,12 +10,12 @@
     file_put_contents( $filename , implode( "\n", $lines ) );
     
     
-    header("Location: http://www.andreasb.one/HUST");
+    header("Location: http://www.andreasb.one/HUST/index.html");
     exit;
     
     
 } else {
-    header("Location: http://www.andreasb.one/HUST");
+    header("Location: http://www.andreasb.one/HUST/index.html");
     exti;
     
 }
